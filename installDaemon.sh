d5dcf262cd3bdc0a72634c3ea4383fbdf3ba0831
rossharper <rossharper@users.noreply.github.com>
2022-01-24 19:44:01 +0000

    Add install script

---
+++ installDaemon.sh
@@ -0,0 +1,9 @@
+read -p 'MQTT Broker Address: ' broker_address
+read -p 'MQTT Broker Username: ' user
+read -sp 'MQTT Broker Password: ' pass
+echo
+read -p 'Sensor Paths: ' sensor_paths
+echo
+
+pm2 start index.js --name files2mqtt -l ~/homecontrol/logs/files2mqtt -- $broker_address -username $user -password $pass $sensor_paths
+pm2 save
